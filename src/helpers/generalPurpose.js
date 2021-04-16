d28e7a1cb1cde5c25560f92472ba4728d8217bc6
Julian <julianrosenthal@yahoo.com>
2021-04-16 15:14:32 -0700

    highlights falsy squares red

--- src/helpers/generalPurpose.js
+++ src/helpers/generalPurpose.js
@@ -1,3 +1,23 @@
 export const uniqueArr = arr => {
   arr.filter((v, i, a) => a.indexOf(v) === i)
 }
+
+export const redHighlighting = (targetAndCandidates) => {
+  const {target, candidates} = targetAndCandidates
+  const className = target.className
+  const redInCN = (className) => className.indexOf('red') > -1 
+
+
+  if (candidates.indexOf(target.value) === -1 && !redInCN(className)) {
+    target.classList.add('red')
+  } 
+  if (target.value === '' && redInCN(className)) {
+    target.classList.remove('red')
+  }
+
+  return target;
+}
+// 3 cases
+// 1 is that target val is '', which is when we remove highlightings
+// 2 is that target val is not in candidates and we add highlights
+// 3 is that target val 
