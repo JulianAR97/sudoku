d0a11a5a4122a05ad0d677c0ab0381f5bacfba91
Julian <julianrosenthal@yahoo.com>
2021-04-19 12:30:06 -0700

    added reducer to set a cell note

--- src/reducers/puzzleReducer.js
+++ src/reducers/puzzleReducer.js
@@ -1,4 +1,4 @@
-
+import { addOrRemoveFromArr } from '../helpers/generalPurpose'
 
 const initialState = {
   puzzle: '',
@@ -7,25 +7,37 @@ const initialState = {
   mode: 'notes', // mode is either notes or input, notes for keeping track of possible inputs
   puzzleArr: [],
   mutables: [],
-  puzzleObj: {}
+  puzzleObj: {},
+  cellNotes: {}
 }
 
 const puzzleReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'SET_PUZZLE':
+      const mutables = action.puzzleInfo.mutables
+      let cellNotes = {};
+      
+      for (let i = 0; i < mutables.length; i++) {
+        let mute = mutables[i]
+        console.log(mute)
+        cellNotes[mute] = []
+      }
+      console.log(cellNotes)
+
       return {
         ...state,
         puzzle: action.puzzleInfo.puzzle,
         solution: action.puzzleInfo.solution,
         puzzleArr: action.puzzleInfo.puzzleArr,
         puzzleObj: action.puzzleInfo.puzzleObj,
-        mutables: action.puzzleInfo.mutables
+        mutables,
+        cellNotes
       }
     
     case 'SET_CURR_SELECTED':
       return {
         ...state,
-        currSelected: action.id
+        currSelected: action.cellID
       }
     
     // Switches mode from input to notes on button click
@@ -34,6 +46,15 @@ const puzzleReducer = (state = initialState, action) => {
       return {
         ...state,
         mode
+      }
+    
+    case 'SET_CELL_NOTE':
+      const {cellID, number} = action.props
+      const prevNoteArr = state.cellNotes[cellID]
+      let noteArr = addOrRemoveFromArr(prevNoteArr, number)
+      return {
+        ...state,
+        cellNotes: {...state.cellNotes, [cellID]: noteArr}
       }
     
     default: 
