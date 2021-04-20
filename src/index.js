4eb8d5527bdefb82fadc7a533499f5f9ac8cd2af
Julian <julianrosenthal@yahoo.com>
2021-04-20 09:33:28 -0700

    imported bootstrap css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 import puzzleReducer from './reducers/puzzleReducer'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(puzzleReducer, composeWithDevTools(applyMiddleware(thunk)))
 ReactDOM.render(
