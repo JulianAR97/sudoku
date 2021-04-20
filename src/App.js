1ea6a939b301bb22167aedb97c9a4757ddeeca34
Julian <julianrosenthal@yahoo.com>
2021-04-20 09:37:25 -0700

    added Navbar to app (root) component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Play from './containers/Play'
-
+import MyNav from './components/MyNav'
 
 const App = () => {
   
   
   return (
-    <Router>
-      <Switch>
-        <Route path="/play" >
-          <Play />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <MyNav />
+      <Router>
+        <Switch>
+          <Route path="/play" >
+            <Play />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   )
 }
 
