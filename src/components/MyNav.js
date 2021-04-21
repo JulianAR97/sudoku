c9763fa9ec3d14e633faaaae54d1e5d4e38e1eaf
Julian <julianrosenthal@yahoo.com>
2021-04-20 18:38:03 -0700

    added github and linkedin icons and added themeing

--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -1,11 +1,25 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import SudokuIcon from '../SudokuIcon.svg'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import '../styles/general.css' 
+
+
+const theme =createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#ba7f9c'
+    }
+  }
+})
+
 
 const MyNav = () => {
   return (
     <Navbar expand="sm" style={{backgroundColor: 'rgb(250, 212, 13)'}}>
+        
         <Navbar.Brand href="/">
           <img
             src={SudokuIcon}
@@ -15,7 +29,7 @@ const MyNav = () => {
             alt="Sudoku Logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" id="main-nav">
             <Nav.Link href="/play">Play</Nav.Link>
@@ -23,6 +37,21 @@ const MyNav = () => {
             <Nav.Link href="/help"></Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+        {/* Only set ml-auto of leftmost icon */}
+        <Navbar.Brand className="ml-auto">
+          <ThemeProvider theme={theme} >
+            <GitHubIcon color="secondary"/>
+          </ThemeProvider>
+
+        </Navbar.Brand>
+        
+        <Navbar.Brand>
+          <LinkedInIcon color="secondary"/>
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+
     </Navbar>
   )
 }
