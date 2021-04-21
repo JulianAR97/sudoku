fa520796890a76a5479a6e2c1252f68a711068c3
Julian <julianrosenthal@yahoo.com>
2021-04-21 16:31:34 -0700

    added user helper file and function to find and set cookies

---
+++ src/helpers/user.js
@@ -0,0 +1,12 @@
+import uuid from 'react-uuid'
+
+export const findOrCreateCookie = () => {
+  if (document.cookie) {
+    return document.cookie
+  } else {
+    const userUUID = `userUUID=${uuid()}`
+    const expiration = `expires=${new Date('01/01/2100').toUTCString()}`
+    document.cookie = `${userUUID};${expiration};SameSite=Lax`;
+    return document.cookie
+  }
+}
