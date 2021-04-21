71ce3788191e05a6e0eedebf0dd67e19866d1ad3
Julian <julianrosenthal@yahoo.com>
2021-04-21 11:14:20 -0700

    minor changes

--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -32,9 +32,10 @@ const MyNav = () => {
         
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" id="main-nav">
-            <Nav.Link href="/play">Play</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/help"></Nav.Link>
+            <Nav.Link href="/play">Play</Nav.Link>
+            <Nav.Link href="/help">Help</Nav.Link>
+            <Nav.Link href="tutorials">Tutorials</Nav.Link>
           </Nav>
         </Navbar.Collapse>
 
