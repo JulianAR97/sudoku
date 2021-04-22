545c89d4cf58942c1016a97afea16a72d25d6a7c
Julian <julianrosenthal@yahoo.com>
2021-04-21 22:13:44 -0700

    sets cookie at top level component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { setCookie } from './actions/puzzleActions'
-import { findOrCreateCookie } from './helpers/user'
+import { findOrCreateCookie, cookieID } from './helpers/user'
 import Play from './containers/Play'
 import MyNav from './components/MyNav'
 
 const App = (props) => {
   
   useEffect(() => {
-    const userUUID = findOrCreateCookie();
+    const userUUID = cookieID(findOrCreateCookie());
     props.setCookie(userUUID)
   })
 
