d680e4d2d0b89b561ab391610496c0cf4fe929e4
Julian <julianrosenthal@yahoo.com>
2021-04-22 11:38:35 -0700

    added async post function

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -12,3 +12,17 @@ export const findOrCreateCookie = () => {
 }
 
 export const cookieID = (cookie) => cookie.split('=')[1]
+
+export const postScore = (time, userUUID) => {
+
+  fetch(`http://localhost:3001/users/${userUUID}/scores`, {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({time})
+  })
+    .then(resp => resp.json())
+
+}
