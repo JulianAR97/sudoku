f577f45347c9c0c7b3f7fc996686a6cccc7b9bbe
Julian <julianrosenthal@yahoo.com>
2021-04-21 19:42:40 -0700

    added ScoreBoard component with dummy return

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,21 @@
+const ScoreBoard = (props) => {
+  
+  return (
+    
+    <div>
+      <h3>Scores</h3>
+      <p>00:00</p>
+      <p>01:01</p>
+      <p>11:11</p>
+      <p>02:03</p>
+      <p>03:05</p>
+      <p>05:06</p>
+      <p>56:59</p>
+      <p>54:05</p>
+      <p>45:45</p>
+    </div>
+
+  )
+}
+
+export default ScoreBoard
