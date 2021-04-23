95997464f54dc7bec2e6db0379186ed0644d8bae
Julian <julianrosenthal@yahoo.com>
2021-04-23 11:39:44 -0700

    added seeds and moved to own file

--- src/helpers/sudokuHelper.js
+++ src/helpers/sudokuHelper.js
@@ -1,3 +1,5 @@
+import {easyPuzzles, mediumPuzzles, hardPuzzles, expertPuzzles} from './seeds'
+
 export const generatePuzzle = (difficulty) => {
   const seed = selectPuzzle(difficulty);
   const conversion = numberMap();
@@ -57,72 +59,9 @@ const selectPuzzle = (difficulty) => {
 
 
 
-// Define some valid sudoku puzzles as 'seeds'
-// Shuffle values and transpose to create 'new' puzzles
 
-//Easy
 
-const e1 = [
-  [0, 2, 4, 9, 0, 3, 6, 0, 0],
-  [0, 3, 0, 7, 0, 5, 0, 4, 9],
-  [5, 0, 0, 6, 0, 0, 0, 7, 0],
-  [0, 0, 3, 4, 9, 1, 0, 0, 7],
-  [4, 0, 1, 0, 5, 0, 8, 0, 3],
-  [0, 0, 6, 3, 7, 0, 0, 0, 1],
-  [0, 0, 0, 0, 4, 7, 0, 8, 0],
-  [8, 0, 2, 5, 0, 0, 0, 0, 0],
-  [0, 0, 7, 0, 0, 2, 0, 3, 5],
-]
 
-const easyPuzzles = [e1];
-
-// Medium
-
-const m1 = [
-  [7, 0, 0, 5, 0, 0, 9, 2, 0],
-  [0, 6, 0, 1, 0, 0, 8, 0, 3],
-  [9, 0, 5, 0, 2, 0, 4, 7, 6],
-  [0, 4, 9, 0, 0, 1, 5, 0, 0],
-  [0, 7, 3, 0, 0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 4, 0, 8, 0],
-  [0, 0, 0, 4, 0, 0, 0, 9, 0],
-  [3, 2, 0, 0, 0, 0, 0, 8, 0],
-  [5, 0, 6, 3, 0, 7, 0, 0, 0],
-]
-
-const mediumPuzzles = [m1];
-
-// Hard 
-
-const h1 = [
-  [0, 4, 0, 0, 9, 0, 8, 0, 0],
-  [0, 0, 9, 0, 2, 3, 4, 0, 0],
-  [6, 3, 0, 7, 8, 0, 5, 0, 0],
-  [0, 0, 0, 9, 0, 0, 0, 0, 0],
-  [0, 0, 0, 8, 0, 0, 0, 0, 0],
-  [0, 7, 4, 0, 0, 0, 0, 0, 3],
-  [5, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 2, 0, 3, 0, 0, 0, 0, 0],
-  [7, 8, 0, 0, 0, 2, 0, 9, 6]
-]
-
-const hardPuzzles = [h1]
-
-// Expert
-
-const ex1 = [
-  [0, 2, 3, 0, 1, 0, 7, 8, 0],
-  [7, 1, 0, 0, 0, 0, 6, 0, 0],
-  [0, 0, 6, 4, 0, 3, 1, 0, 0],
-  [0, 0, 0, 3, 2, 0, 0, 0, 0],
-  [0, 3, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 8, 0, 0, 0, 0],
-  [0, 0, 0, 1, 5, 0, 0, 7, 0],
-  [0, 0, 0, 0, 0, 0, 8, 3, 0],
-  [8, 0, 4, 7, 0, 0, 5, 2, 0],
-]
-
-const expertPuzzles = [ex1]
 
 const diffToPuzzles = {
   'easy': easyPuzzles,
