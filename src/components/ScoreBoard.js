5005e57ab4b8e2984cea274767d3af8b6b1b9f1b
Julian <julianrosenthal@yahoo.com>
2021-04-25 20:54:49 -0700

    minor refactoring

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -2,20 +2,30 @@ import React from 'react';
 
 
 const ScoreBoard = (props) => {
-  
-  const renderScores = (scores) => {
-    if (scores[0] === null) {
+  let {difficulty, stats} = props
+  const renderScores = () => {
+    let scores = stats["topTenByDifficulty"] && difficulty ? stats["topTenByDifficulty"][difficulty] : []
+    if (!scores[0]) {
       return <p>No scores to display...</p>
     } else {
       return scores.map(score => <p>{score}</p>)  
     }
   }
 
+  const renderDifficulty = () => {
+    if (difficulty) {
+      return (
+        <h5>({difficulty})</h5>
+      )
+    }
+  }
+
   return (
     
     <div>
-      <h3>Scores</h3>
-      {renderScores(props.scores)}
+      <h3>Top Scores</h3>
+      {renderDifficulty()}
+      {renderScores()}
     </div>
 
   )
